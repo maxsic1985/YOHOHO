44b6ed1bcf68062a51a3b347b8b557fb250a1f93
Maksim S <maxsic1985@gmail.com>
2024-06-14 18:50:34 +0300

    upd

--- Assets/Code/Systems/EnemySystems/EnemyDeathSystem.cs
+++ Assets/Code/Systems/EnemySystems/EnemyDeathSystem.cs
@@ -1,6 +1,7 @@
 ﻿using Leopotam.EcsLite;
+using Leopotam.EcsLite.Unity.Ugui;
 using LeopotamGroup.Globals;
-
+using TMPro;
 
 
 namespace MSuhininTestovoe.B2B
@@ -13,7 +14,8 @@ namespace MSuhininTestovoe.B2B
         private EcsPool<TransformComponent> _transformComponentPool;
         private EcsPool<IsDropInstantiateFlag> _isDropComponentPool;
         private IPoolService _poolService;
-
+        private int _deathEnemyCnt=0;
+        [EcsUguiNamed(UIConstants.ENEMY_CNT)] readonly TextMeshProUGUI _enemyCntlabel = default;
 
         public void Init(IEcsSystems systems)
         {
@@ -28,6 +30,8 @@ namespace MSuhininTestovoe.B2B
             _enemyHealthComponentPool = _world.GetPool<EnemyHealthComponent>();
             _transformComponentPool = _world.GetPool<TransformComponent>();
             _isDropComponentPool = _world.GetPool<IsDropInstantiateFlag>();
+
+            _enemyCntlabel.text = _deathEnemyCnt.ToString();
         }
 
         
@@ -42,6 +46,8 @@ namespace MSuhininTestovoe.B2B
                     ref IsDropInstantiateFlag drop = ref _isDropComponentPool.Add(transform.Value.gameObject.GetComponent<EnemyActor>().Entity);
                     _poolService.Return(transform.Value.gameObject);
                     health.HealthValue = 3;
+                    _deathEnemyCnt += 1;
+                    _enemyCntlabel.text = _deathEnemyCnt.ToString();
                 }
             }
         }
